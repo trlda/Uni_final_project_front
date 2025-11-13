e99abe38563b9260d1bb7d31b0339eb428d84707
mihailtimosin <m.timoshyn@ukma.edu.ua>
2025-11-13 14:36:04 +0200

    Update

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
     const connectButtons = document.querySelectorAll('.connect-btn');
 
-    connectButtons.forEach(button => {
-        button.addEventListener('click', function() {
-            const currency = this.getAttribute('data-currency');
-            const addressInput = document.getElementById(`${currency}Address`);
-            const balanceDisplay = document.getElementById(`${currency}Balance`);
+    connectButtons.forEach(function(button) {
+        button.addEventListener('click', async function() {
+            const symbol = this.getAttribute('data-symbol');
+            const addressInput = document.getElementById(`${symbol}Address`);
+            const balanceDisplay = document.getElementById(`${symbol}Balance`);
 
             const address = addressInput.value.trim();
 
             if (!address) {
-                alert(`Input address ${currency} wallet`);
+                alert(`Input address ${symbol} wallet`);
                 return;
             }
             balanceDisplay.textContent = 'Loading...';
 
-            checkBalance(currency, address, balanceDisplay);
+            try {
+                const url = `http://127.0.0.1:8000/balance/?symbol=${symbol}&address=${address}`;
+                const response = await fetch(url);
+                const data = await response.json();
+                balanceDisplay.textContent = `Balance: ${data.balance} ${data.symbol}`;
+            } catch (error) {
+                console.error(error);
+                balanceDisplay.textContent = 'Error loading balance';
+            }
         });
     });
-
-    function checkBalance(currency, address, balanceDisplay) {
-        const url = `http://127.0.0.1:8000/balance/?symbol=${currency}&address=${address}`;
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                balanceDisplay.textContent = `Balance: ${data.balance} ${data.symbol}`;
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                balanceDisplay.textContent = 'Error loading balance';
-            });
-    }
 });
